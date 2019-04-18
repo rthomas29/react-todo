51fb3bb8c2055a1950a5fc2af851b51c4e1c0ec2
Rakeem <thomas.rakeem.29@gmail.com>
2019-04-18 09:41:07 -0400

    :art: Formatting changes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react'
 
-const Button = ({ className = 'btn btn-primary', type = 'button', onClick = () => { }, children }) => {
+const Button = ({
+  className = 'btn btn-primary',
+  type = 'button',
+  onClick = () => { },
+  style = {},
+  children
+}) => {
   return (
     <Fragment>
-      <button type={type} className={className} onClick={() => onClick()}>
+      <button type={type} className={className} style={style} onClick={() => onClick()}>
         {children}
       </button>
     </Fragment>
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,40 +2,54 @@ import React from 'react'
 import Button from '../Button'
 import './styles.css'
 
-const TodoList = ({ todos, handleTaskDelete, toggleTaskCompletion, deleteCompletedTasks, numTasks }) => (
-  <main className='list-container'>
-    {
-      todos.map((todo, i) => {
-        const listNumber = i + 1
-        const key = `${i}:${todo.task}`
-        const style = todo.isComplete ? { textDecoration: 'line-through' } : {}
-        const isChecked = todo.isComplete ? todo.isComplete : false
-        return (
-          <div
-            key={key}
-            className='list-container'
-          >
-            <div className='list-item'>
-              <input
-                type='checkbox'
-                className='form-check-input'
-                checked={isChecked}
-                onChange={() => toggleTaskCompletion(i)}
-              />
-              <p style={style}>
-                {`${listNumber}. ${todo.task}`}
-              </p>
+const TodoList = ({
+  todos,
+  handleTaskDelete,
+  toggleTaskCompletion,
+  deleteCompletedTasks,
+  toggleAllTasksCompletion,
+  allMarkedComplete,
+  numTasks
+}) => {
+  const markedCompleteBtnText = allMarkedComplete ? 'Unselect tasks' : 'Select all tasks'
+  return (
+    <main className='list-container'>
+      {
+        todos.map((todo, i) => {
+          const listNumber = i + 1
+          const key = `${i}:${todo.task}`
+          const style = todo.isComplete ? { textDecoration: 'line-through' } : {}
+          const isChecked = todo.isComplete ? todo.isComplete : false
+          return (
+            <div
+              key={key}
+              className='list-container'
+            >
+              <div className='list-item'>
+                <input
+                  type='checkbox'
+                  className='form-check-input'
+                  checked={isChecked}
+                  onChange={() => toggleTaskCompletion(i)}
+                />
+                <p style={style}>
+                  {`${listNumber}. ${todo.task}`}
+                </p>
+              </div>
             </div>
-          </div>
-        )
-      })
-    }
-    {numTasks > 0 &&
-      <Button className='btn btn-danger' onClick={deleteCompletedTasks}>
-        Clear completed tasks
-        </Button>
-    }
-  </main>
-)
+          )
+        })
+      }
+      {numTasks > 0 &&
+        <div className='btn-group'>
+          <Button className='btn btn-sm btn-danger' onClick={deleteCompletedTasks}>
+            Clear completed tasks
+          </Button>
+          <Button className='btn btn-sm btn-info' onClick={toggleAllTasksCompletion}>{markedCompleteBtnText}</Button>
+        </div>
+      }
+    </main>
+  )
+}
 
 export default TodoList
